9821a4b5e3f7e5d49c010304b24b49e2d8fa6d1e
Stephane Girão <stephanegiraolinhares@gmail.com>
2024-06-19 09:19:38 -0300

    first commit with the Xcode project

---
+++ Ping/Ping/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ping
+//
+//  Created by Stephane Girão Linhares on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! Let's go make a ping plugin S2")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ping/Ping/PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingApp.swift
+//  Ping
+//
+//  Created by Stephane Girão Linhares on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
