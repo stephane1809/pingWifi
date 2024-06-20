2012c719ed21dc837a6780d568f3140d0e7c8a17
Stephane Girão <stephanegiraolinhares@gmail.com>
2024-06-20 09:40:52 -0300

    get information if the device ias connect with wifi in the firt time you open the app

--- Ping/Ping/ContentView.swift
+++ Ping/Ping/ContentView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import Network
 
 struct ContentView: View {
+//    let monitor: NWPathMonitor
+//
+//    public func startMonitoring() {
+//        monitor.pathUpdateHandler = { path in
+//            if path.status == .satisfied {
+//                print("Internet connection is available.")
+//                // Perform actions when internet is available
+//            } else {
+//                print("Internet connection is not available.")
+//                // Perform actions when internet is not available
+//            }
+//        }
+//    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -17,6 +32,27 @@ struct ContentView: View {
         }
         .padding()
     }
+}
+
+final class NetworkMonitor {
+    static let shered = NetworkMonitor()
+    private let queue = DispatchQueue.global()
+    private let monitor: NWPathMonitor
+    public private(set) var isConnected: Bool = false
+
+
+    private init() {
+        monitor = NWPathMonitor()
+    }
+
+    public func startMonitoring() {
+
+    }
+
+    public func stopMonitoring() {
+        monitor.cancel()
+    }
+
 }
 
 #Preview {
